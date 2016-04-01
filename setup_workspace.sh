b37ea66c65c202c74c82b40da80d1cbf1f91a0ed
Sean Williams <unixed@gmail.com>
2016-04-01 15:57:59 -0700

    Script to set up go workspace and project

---
+++ setup_workspace.sh
@@ -0,0 +1,8 @@
+path="$HOME/workspace.$RANDOM"
+export GOPATH=$path
+export GOBIN=$path/bin
+mkdir -p $path/src
+cd $path/src
+go get github.com/unixist/cryptostalker
+go install github.com/unixist/cryptostalker
+echo -e 'Now you can run:\n  $GOBIN/cryptostalker --path=/tmp'
